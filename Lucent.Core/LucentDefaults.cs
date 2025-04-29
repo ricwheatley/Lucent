96808c00629d68cd1431861a62c07000b7525b7a
Ric <ric@brighteraccounting.com>
2025-04-29 06:09:23 +0100

    feat: centralise resilience & token defaults

---
+++ Lucent.Core/LucentDefaults.cs
@@ -0,0 +1,12 @@
+﻿namespace Lucent.Core;
+
+public static class LucentDefaults
+{
+    public const int TokenEarlyExpirySeconds = 300;
+
+    public const int HttpRetryCount = 3;
+    public static readonly TimeSpan HttpBaseDelay = TimeSpan.FromSeconds(2);
+
+    public const int SqlRetryCount = 5;
+    public static readonly TimeSpan SqlBaseDelay = TimeSpan.FromMilliseconds(250);
+}
